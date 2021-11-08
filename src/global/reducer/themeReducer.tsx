1f17a9054876fc116c748ef5f7f08ca27b7a1d79
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-11-08 22:58:27 +0500

    lags

--- src/global/reducer/themeReducer.tsx
+++ src/global/reducer/themeReducer.tsx
@@ -9,7 +9,20 @@ const initialState = {
 
 const pref = matchMedia("(prefers-color-scheme: dark)")
 console.log(pref);
-matchMedia
+matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => {
+    console.log(e.matches);
+    if (e.matches) {
+        console.log('dark');
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('preferred-theme', 'dark');
+        }
+    } else {
+        console.log('light');
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('preferred-theme', 'light');
+        }
+    }
+});
 
 console.log('init',initialState);
 const theme = initialState.theme;
