38f68d89dee02137cffb025633a1554ab0a1a725
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-11-09 00:34:17 +0500

    reverted

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,21 +11,21 @@ export const wrapPageElement = ({ element }) => {
     return <Layout>{element}</Layout>
 }
 
-export const onRenderBody = ({ setPostBodyComponents }) => {
-    setPostBodyComponents([
-        <script
-            key="gatsby-dark-mode-mui"
-            dangerouslySetInnerHTML={{
-                __html: `
-                    window.__setTheme = (theme) => {
-                        localStorage.setItem('preferred-theme', theme);
-                    };
-                    window.__getTheme = () => {
-                        return localStorage.getItem('preferred-theme') || 'light';
-                    };
-                `,
-            }}
-        />,
-    ]);
-};
+// export const onRenderBody = ({ setPostBodyComponents }) => {
+//     setPostBodyComponents([
+//         <script
+//             key="gatsby-dark-mode-mui"
+//             dangerouslySetInnerHTML={{
+//                 __html: `
+//                     window.__setTheme = (theme) => {
+//                         localStorage.setItem('preferred-theme', theme);
+//                     };
+//                     window.__getTheme = () => {
+//                         return localStorage.getItem('preferred-theme') || 'light';
+//                     };
+//                 `,
+//             }}
+//         />,
+//     ]);
+// };
             
