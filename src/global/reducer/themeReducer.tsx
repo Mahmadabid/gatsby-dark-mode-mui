c8286b2f007546a53c1e718a98344a3fc7082095
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-11-09 13:26:52 +0500

    added dark mode

--- src/global/reducer/themeReducer.tsx
+++ src/global/reducer/themeReducer.tsx
@@ -1,13 +1,10 @@
 import { ThemeProvider } from "@mui/material";
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import { darkTheme, lightTheme } from "../theme/theme";
 import { stateProps, themeActionProps, themeActionTypes } from "../types/reducerTypes";
 
-// const themeFetch = typeof window !== 'undefined' ? localStorage.getItem('preferred-theme') ? localStorage.getItem('preferred-theme') : 'light' : 'light';
-
 const initialState = {
     theme: 'light'
-    // theme: themeFetch
 };
 
 const themeReducer = (state: stateProps, action: themeActionProps) => {
@@ -28,6 +25,9 @@ export const GlobalDispatchContext = createContext<React.Dispatch<themeActionPro
 const ThemesProvider = ({ children }) => {
 
     const [theme, setTheme] = React.useState('light');
+    const [switched, setSwitch] = React.useState(true);
+
+    // Fetch localStorage and set it to theme
     React.useEffect(() => {
         const localTheme = localStorage.getItem('preferred-theme');
         if (localTheme) {
@@ -36,32 +36,16 @@ const ThemesProvider = ({ children }) => {
         else {
             setTheme('light');
         }
+        setSwitch(false);
     }, []);
-console.log(theme);
 
     // set the initialSate equal to the localStorage theme
     initialState.theme = theme
-
+    
     const [state, dispatch] = useReducer(themeReducer, initialState);
 
-    // useEffect(() => {
-    //     themeFetch === 'dark' ? setTheme(1) : setTheme(0);
-    //     state.theme === 'dark' ? setTheme(1) : setTheme(0);
-    // } , [state.theme]);
-
-    // To change the preferred theme
-    // let theme = 'light';
-
-    // if (typeof window !== 'undefined') {
-    //     theme = localStorage.getItem('preferred-theme');        
-    //     theme = theme ? theme : 'light';
-    //     state.theme = theme        
-    // }
-console.log(2,theme);
-
     return (
-        // <ThemeProvider theme={theme === 1 ? darkTheme : state.theme === 'light' ? lightTheme : darkTheme}>
-        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <ThemeProvider theme={switched ? theme === 'light' ? lightTheme : darkTheme : state.theme === 'light' ? lightTheme : darkTheme}>
             <GlobalStateContext.Provider value={state}>
                 <GlobalDispatchContext.Provider value={dispatch}>
                     {children}
